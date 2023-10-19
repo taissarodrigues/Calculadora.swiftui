14265c1de9ab0244641add23dc9f2c6b4802dd4f
taissa-rodrigues <taissavrp@gmail.com>
2023-10-19 13:34:14 -0300

    Initial Commit

---
+++ Calculadora.swift/Calculadora_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora_swiftApp.swift
+//  Calculadora.swift
+//
+//  Created by user on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
